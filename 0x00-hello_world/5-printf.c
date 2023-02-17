3e251ad420d3b8d2b07995ad6e3bd78d7ce32257
Temdamsel <otemoyetemitayo@gmail.com>
2023-02-17 13:40:35 -0800

    world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry point 
- *
- * Return: Always 0 (Success)
- */
+* main - print a string
+*
+* Return: text in printf
+*/
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
