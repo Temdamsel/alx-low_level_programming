0cc02dfb1a44be12037c7a4f869127e871736773
Temdamsel <otemoyetemitayo@gmail.com>
2023-02-19 14:08:01 -0800

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,22 +9,18 @@ int main(void)
 {
 	int c, i;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = '0'; c < '9'; c++)
 	{
-	for (i = '0'; i <= '9'; i++)
+	for (i = c + 1; i < '10'; i++)
 	{
-	if (c < i)
-	{
-		putchar(c);
-		putchar(i);
+		putchar((c % 10) + '0');
+		putchar((i % 10) + '0');
 
-	if (c != '8' || (c == '8' && i != '9'))
-	{
+	if (c == '8' && i != '9')
+continue:
 	putchar(',');
 	putchar('');
 	}
-	}
-}
 }
 		putchar('\n');
 		return (0);
