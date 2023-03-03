04d93b69dfe5d7473874f4229e9e42f353df2cfd
Temdamsel <otemoyetemitayo@gmail.com>
2023-03-02 16:28:51 -0800

    cat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
@@ -14,5 +14,4 @@ char *rot13(char *a);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
 #endif
